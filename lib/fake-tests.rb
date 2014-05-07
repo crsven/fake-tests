f90bd17e4011b7fa18faa1292f2c231a5530e73c
Chris Svenningsen <chris@carbonfive.com>
2014-05-07 09:26:25 -0700

    Add header.

--- lib/fake-tests.rb
+++ lib/fake-tests.rb
@@ -4,9 +4,17 @@ class FakeTests
   end
 
   def run!
+    output_header
     while @running do
       print '.'
       sleep(1)
     end
+  end
+
+  def output_header
+    puts 'rails            11.720000   2.300000  14.020000 ( 16.012029)'
+    sleep(2)
+    puts 'support           0.670000   0.100000   0.770000 (  3.675325)'
+    sleep(2)
   end
 end
